31ca210fe8ba00df232e4808ffa2c0147779c4de
Stepan Andreev <stepan.andreev@gmail.com>
2016-04-20 08:13:06 +0100

    AssetLoader fix

--- IRSequencer/IRSequencer/Gui/UIAssetsLoader.cs
+++ IRSequencer/IRSequencer/Gui/UIAssetsLoader.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 
 namespace IRSequencer.Gui
 {
-    [KSPAddon(KSPAddon.Startup.Instantly, false)]
+    [KSPAddon(KSPAddon.Startup.MainMenu, false)]
     public class UIAssetsLoader : MonoBehaviour
     {
         private AssetBundle IRAssetBundle;
@@ -38,102 +38,121 @@ namespace IRSequencer.Gui
             {
                 yield return www;
                 IRAssetBundle = www.assetBundle;
-                var prefabs = IRAssetBundle.LoadAllAssets<GameObject>();
-                int prefabsLoadedCount = 0;
-                for (int i=0; i< prefabs.Length; i++)
-                {
-                    if(prefabs[i].name == "SequencerMainWindowPrefab")
-                    {
-                        controlWindowPrefab = prefabs[i] as GameObject;
-                        prefabsLoadedCount++;
-                    }
-                    if (prefabs[i].name == "SequencerLinePrefab")
-                    {
-                        sequencerLinePrefab = prefabs[i] as GameObject;
-                        prefabsLoadedCount++;
-                    }
 
-                    if (prefabs[i].name == "SequencerStateLinePrefab")
-                    {
-                        stateLinePrefab = prefabs[i] as GameObject;
-                        prefabsLoadedCount++;
-                    }
-
-                    if (prefabs[i].name == "SequenceLinePrefab")
-                    {
-                        sequenceLinePrefab = prefabs[i] as GameObject;
-                        prefabsLoadedCount++;
-                    }
-
-                    if (prefabs[i].name == "UISettingsWindowPrefab")
-                    {
-                        uiSettingsWindowPrefab = prefabs[i] as GameObject;
-                        prefabsLoadedCount++;
-                    }
-
-                    if (prefabs[i].name == "SequencerEditorWindowPrefab")
-                    {
-                        editorWindowPrefab = prefabs[i] as GameObject;
-                        prefabsLoadedCount++;
-                    }
-
-                    if (prefabs[i].name == "SequenceCommandLine")
-                    {
-                        sequenceCommandLinePrefab = prefabs[i] as GameObject;
-                        prefabsLoadedCount++;
-                    }
-
-                    if (prefabs[i].name == "BasicTooltipPrefab")
-                    {
-                        basicTooltipPrefab = prefabs[i] as GameObject;
-                        prefabsLoadedCount++;
-                    }
-                }
-
-                allPrefabsReady = (prefabsLoadedCount > 7);
-
-                spriteAssets = new List<UnityEngine.Sprite>();
-                var sprites = IRAssetBundle.LoadAllAssets<UnityEngine.Sprite>();
-
-                for (int i = 0; i < sprites.Length; i++)
-                {
-                    if (sprites[i] != null)
-                    {
-                        spriteAssets.Add(sprites[i]);
-                        Logger.Log("Successfully loaded Sprite " + sprites[i].name);
-                    }
-                }
-
-                iconAssets = new List<Texture2D>();
-                var icons = IRAssetBundle.LoadAllAssets<Texture2D>();
-
-                for (int i = 0; i < icons.Length; i++)
-                {
-                    if (icons[i] != null)
-                    {
-                        iconAssets.Add(icons[i]);
-                        Logger.Log("Successfully loaded texture " + icons[i].name);
-                    }
-                    
-                }
+                LoadBundleAssets();
 
                 IRAssetBundle.Unload(false);
             }
         }
         
+        private void LoadBundleAssets()
+        {
+            var prefabs = IRAssetBundle.LoadAllAssets<GameObject>();
+            int prefabsLoadedCount = 0;
+            for (int i = 0; i < prefabs.Length; i++)
+            {
+                if (prefabs[i].name == "SequencerMainWindowPrefab")
+                {
+                    controlWindowPrefab = prefabs[i] as GameObject;
+                    prefabsLoadedCount++;
+                }
+                if (prefabs[i].name == "SequencerLinePrefab")
+                {
+                    sequencerLinePrefab = prefabs[i] as GameObject;
+                    prefabsLoadedCount++;
+                }
+
+                if (prefabs[i].name == "SequencerStateLinePrefab")
+                {
+                    stateLinePrefab = prefabs[i] as GameObject;
+                    prefabsLoadedCount++;
+                }
+
+                if (prefabs[i].name == "SequenceLinePrefab")
+                {
+                    sequenceLinePrefab = prefabs[i] as GameObject;
+                    prefabsLoadedCount++;
+                }
+
+                if (prefabs[i].name == "UISettingsWindowPrefab")
+                {
+                    uiSettingsWindowPrefab = prefabs[i] as GameObject;
+                    prefabsLoadedCount++;
+                }
+
+                if (prefabs[i].name == "SequencerEditorWindowPrefab")
+                {
+                    editorWindowPrefab = prefabs[i] as GameObject;
+                    prefabsLoadedCount++;
+                }
+
+                if (prefabs[i].name == "SequenceCommandLine")
+                {
+                    sequenceCommandLinePrefab = prefabs[i] as GameObject;
+                    prefabsLoadedCount++;
+                }
+
+                if (prefabs[i].name == "BasicTooltipPrefab")
+                {
+                    basicTooltipPrefab = prefabs[i] as GameObject;
+                    prefabsLoadedCount++;
+                }
+            }
+
+            allPrefabsReady = (prefabsLoadedCount > 7);
+
+            spriteAssets = new List<UnityEngine.Sprite>();
+            var sprites = IRAssetBundle.LoadAllAssets<UnityEngine.Sprite>();
+
+            for (int i = 0; i < sprites.Length; i++)
+            {
+                if (sprites[i] != null)
+                {
+                    spriteAssets.Add(sprites[i]);
+                    Logger.Log("Successfully loaded Sprite " + sprites[i].name);
+                }
+            }
+
+            iconAssets = new List<Texture2D>();
+            var icons = IRAssetBundle.LoadAllAssets<Texture2D>();
+
+            for (int i = 0; i < icons.Length; i++)
+            {
+                if (icons[i] != null)
+                {
+                    iconAssets.Add(icons[i]);
+                    Logger.Log("Successfully loaded texture " + icons[i].name);
+                }
+
+            }
+        }
+
+        public void LoadBundleFromDisk(string path)
+        {
+            IRAssetBundle = AssetBundle.CreateFromFile(path);
+
+            LoadBundleAssets();
+
+            IRAssetBundle.Unload(false);
+        }
+
+
         public void Start()
         {
             var assemblyFile = Assembly.GetExecutingAssembly().Location;
             //we will use same path for AssetBundles as IR and share some assets
             var bundlePath = "file://" + assemblyFile.Replace(new FileInfo(assemblyFile).Name, "").Replace("\\","/") + "../../AssetBundles/";
+            var filePath = assemblyFile.Replace(new FileInfo(assemblyFile).Name, "") + "../../AssetBundles/";
 
-            Logger.Log("Loading bundles from BundlePath: " + bundlePath);
+            //Logger.Log("Loading bundles from BundlePath: " + bundlePath, Logger.Level.Debug);
 
             //need to clean cache
-            Caching.CleanCache();
-            
-            StartCoroutine(LoadBundle(bundlePath + "ir_ui_objects.ksp"));
-           
+            //Caching.CleanCache();
+
+            //StartCoroutine(LoadBundle(bundlePath + "ir_ui_objects.ksp"));
+
+            Logger.Log("Loading bundles from filePath: " + filePath, Logger.Level.Debug);
+            LoadBundleFromDisk(filePath + "ir_ui_objects.ksp");
         }
     }
 }
